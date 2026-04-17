864dac18c167768f20748264b11c6b4f3bf062a0
Torben Lenz <torben@lemontree.local>
2026-04-17 18:05:34 +0200

    feat: CSS-Grundlage und Navigation JS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.nav__toggle');
+  const links = document.querySelector('.nav__links');
+  if (toggle && links) {
+    toggle.addEventListener('click', () => links.classList.toggle('open'));
+  }
+});
